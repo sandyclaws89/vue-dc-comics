f776d7b7c984ae4e083cf63f213b44b1db5a9d57
sandyclaws89 <96080101+sandyclaws89@users.noreply.github.com>
2022-03-22 17:06:26 +0100

    creo struttura

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,22 @@
 <template>
   <div id="app">
-   
+   <HeaderDc/>
     
   </div>
 </template>
 
 <script>
 
+import HeaderDc from './components/HeaderDc.vue'
 
 export default {
   name: 'App',
   components: {
-  }
+        HeaderDc
+    },
 }
 </script>
 
 <style lang= "scss">
-#app {
-  font-family: Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/HeaderDc.vue
@@ -0,0 +1,71 @@
+<template>
+<!-- NEL TEMPLATE CI METTO TUTTO IL MIO HTML.  -->
+    <header>
+        <img src="../assets/img/dc-logo.png" alt="">
+        <ul>
+            <li v-for="(element, index) in links" :key="index">
+                <a :href="element.href">{{element.text}} </a>
+            </li>   
+        </ul>
+    </header>
+</template>
+
+<script>
+
+export default {
+    name: 'HeaderDc',
+    data () {
+        return {
+            links: [
+                {
+                    href: '#',
+                    text: 'CHARACTERS'
+                },
+                {
+                    href: '#',
+                    text: 'COMICS'
+                },
+                {
+                    href: '#',
+                    text: 'MOVIES'
+                },
+                {
+                    href: '#',
+                    text: 'TV'
+                },
+                {
+                    href: '#',
+                    text: 'GAMES'
+                },
+                {
+                    href: '#',
+                    text: 'COLLECTIBLES'
+                },
+                {
+                    href: '#',
+                    text: 'VIDEOS'
+                },
+                {
+                    href: '#',
+                    text: 'FANS'
+                },
+                {
+                    href: '#',
+                    text: 'NEWS'
+                },
+                 {
+                    href: '#',
+                    text: 'SHOP'
+                },
+
+
+
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
